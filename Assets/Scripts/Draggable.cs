9766d04e360b855c3aa52dfd4da9b89f4e9ac589
LOWERCASE <lowercase.inbox@gmail.com>
2021-08-20 16:59:23 -0400

    :b:ush

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -4,40 +4,41 @@ using UnityEngine;
 
 public class Draggable : MonoBehaviour
 {
-    [SerializeField]
-    bool isHeld;
-    [SerializeField]
-    Grid grid;
-
-    private void Start() {
-        grid = Synchronizer.Self.grid;
-    }
-
-    private void Update() {
-        if (isHeld) {
-            Vector3 mousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0f);
-            transform.position = grid.GetCellCenterLocal(grid.WorldToCell(mousePos));
-        }
-    }
-
-    private void OnMouseDown() {
-        PickUp();
-    }
-
-    private void OnMouseUp() {
-        PutDown();
-    }
-
-    public void PickUp () {
-        gameObject.layer = 6;
-        print("picked up");
-        isHeld = true;
-    }
-
-    void PutDown () {
-        gameObject.layer = 0;
-        print("put down");
-        isHeld = false;
-    }
-
+	[SerializeField]
+	bool isHeld;
+	[SerializeField]
+	Grid grid;
+	
+	private void Start() {
+		grid = Synchronizer.Self.grid;
+	}
+	
+	private void Update() {
+		if (isHeld) {
+			Vector3 mousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0f);
+			transform.position = grid.GetCellCenterLocal(grid.WorldToCell(mousePos));
+		}
+	}
+	
+	private void OnMouseDown() {
+		PickUp();
+	}
+	
+	private void OnMouseUp() {
+		PutDown();
+	}
+	
+	public void PickUp() {
+		gameObject.layer = 6;
+		print("picked up");
+		isHeld = true;
+	}
+	
+	void PutDown() {
+		
+		gameObject.layer = 0;
+		print("put down");
+		isHeld = false;
+	}
+	
 }

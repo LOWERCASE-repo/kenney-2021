2a854c81dd195d72282bdb12604fdd4eef2d17a6
LOWERCASE <lowercase.inbox@gmail.com>
2021-08-20 17:29:59 -0400

    polling works

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Draggable : MonoBehaviour
 {
@@ -8,24 +9,44 @@ public class Draggable : MonoBehaviour
 	bool isHeld;
 	[SerializeField]
 	Grid grid;
+	[SerializeField]
+	Tilemap placements;
+	[SerializeField]
+	SpriteRenderer sprite;
+	
+	Camera cam;
+	Vector3Int poll;
+	
+	internal Generator generator;
 	
 	private void Start() {
+		cam = Camera.main;
+		sprite = GetComponent<SpriteRenderer>();
 		grid = Synchronizer.Self.grid;
+		placements = grid.transform.GetChild(1).GetComponent<Tilemap>();
 	}
 	
 	private void Update() {
 		if (isHeld) {
-			Vector3 mousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0f);
-			transform.position = grid.GetCellCenterLocal(grid.WorldToCell(mousePos));
+			Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+			mousePos.z = 0f;
+			poll = grid.WorldToCell(mousePos);
+			transform.position = grid.GetCellCenterLocal(poll);
 		}
 	}
 	
 	private void OnMouseDown() {
+		sprite.sortingLayerName = "Placements";
 		PickUp();
 	}
 	
 	private void OnMouseUp() {
-		PutDown();
+		sprite.sortingLayerName = "Default";
+		if (placements.GetTile(poll) != null) PutDown();
+		else {
+			Destroy(gameObject);
+			generator.IncrementTNH();
+		}
 	}
 	
 	public void PickUp() {
@@ -35,7 +56,6 @@ public class Draggable : MonoBehaviour
 	}
 	
 	void PutDown() {
-		
 		gameObject.layer = 0;
 		print("put down");
 		isHeld = false;

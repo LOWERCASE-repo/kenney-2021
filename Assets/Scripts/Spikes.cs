432745d1c06087123d2de82191582e78b99f4fe1
LOWERCASE <lowercase.inbox@gmail.com>
2021-08-21 12:04:33 -0400

    the REAL eeby deeby

--- Assets/Scripts/Spikes.cs
+++ Assets/Scripts/Spikes.cs
@@ -12,7 +12,7 @@ class Spikes : MonoBehaviour {
 		pops[index].transform.position = collider.transform.position;
 		pops[index].SetActive(true);
 		index = (index + 1) % pops.Length;
-		collider.transform.position = Vector3.down * 100f;
+		collider.transform.position = Vector3.down * 1085072238237f;
 		source.PlayOneShot(sounds[Random.Range(0, sounds.Length)]);
 	}
 }
